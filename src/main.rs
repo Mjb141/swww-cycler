946131c0c44cbde24b09d52bb24cc0f62f550afe
Mjb141 <46346590+Mjb141@users.noreply.github.com>
2023-08-06 23:12:12 +0100

    Working draft 1
    First working version of background change on workspace change

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use anyhow::{Context, Ok};
+use env_logger::Env;
+use glob::glob;
+use log::{debug, error, info};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+use which::which;
+
+use clap::Parser;
+
+use hyprland::event_listener::EventListener;
+use hyprland::shared::WorkspaceType;
+
+const SWWW_BINARY: &str = "swww";
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    #[arg(long)]
+    pub backgrounds_path: String,
+}
+
+fn main() {
+    let env = Env::default().filter_or("SWWW_CYCLER_LOG_LEVEL", "info");
+    env_logger::init_from_env(env);
+
+    if let Err(_) = which(SWWW_BINARY) {
+        error!("'{SWWW_BINARY}' binary not found on PATH");
+        return;
+    }
+    debug!("'{SWWW_BINARY}' binary found on PATH");
+
+    let mut event_listener = EventListener::new();
+    event_listener.add_workspace_change_handler(|data| handle_workspace_change(data));
+    event_listener.start_listener().unwrap();
+}
+
+fn handle_workspace_change(data: WorkspaceType) {
+    match data {
+        WorkspaceType::Regular(reg_workspace_num) => {
+            info!(
+                "Workspace change (Regular) to workspace {:?}",
+                reg_workspace_num
+            );
+            let background_path = get_random_background_image().unwrap();
+            info!("Background selected: {:?}", background_path);
+            change_background(background_path);
+        }
+        _ => {
+            debug!("Workspace change event (Special) ignored");
+        }
+    }
+}
+
+fn get_random_background_image() -> anyhow::Result<PathBuf> {
+    let args = Args::parse();
+    let backgrounds_dir = Path::new(&args.backgrounds_path);
+    if !backgrounds_dir.exists() | !backgrounds_dir.is_dir() {
+        error!("Backgrounds directory {:?} not found", backgrounds_dir);
+    };
+
+    // See: https://docs.rs/glob/latest/glob/fn.glob.html#examples
+    let backgrounds_paths: Vec<PathBuf> = glob(&format!("{}/*", args.backgrounds_path))
+        .unwrap()
+        .filter_map(Result::ok)
+        .collect();
+
+    let mut rng = thread_rng();
+    let chosen_background = backgrounds_paths
+        .choose(&mut rng)
+        .context("Couldn't choose a background")?;
+    Ok(chosen_background.clone())
+}
+
+fn change_background(background_path: PathBuf) {
+    let path_str = background_path.as_path().to_str().unwrap();
+    info!("{path_str}");
+    let output = Command::new("swww")
+        .args(["img", path_str])
+        .output()
+        .unwrap();
+    info!("{:?}", output.stdout);
+}
