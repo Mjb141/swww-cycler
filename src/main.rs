a340792351359d998d03ba02e20bd108de0108ff
Mjb141 <mike141@live.com>
2023-08-12 15:52:18 +0100

    Simplified error handling, variants, and logging

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use clap::Parser;
 use env_logger::Env;
 use hyprland::event_listener::EventListener;
 use hyprland::shared::WorkspaceType;
-use log::{debug, error, warn};
+use log::{debug, error};
 use rand::{seq::SliceRandom, thread_rng};
 use std::{path::PathBuf, process::Command};
 use which::which;
 
 use crate::utils::get_valid_image_paths_from_provided_dir;
+use crate::utils::ParsingError;
 
 const SWWW_BINARY: &str = "swww";
 
@@ -21,47 +22,36 @@ pub struct Args {
 
 pub fn handle_workspace_change(data: WorkspaceType, valid_image_paths: &Vec<PathBuf>) {
     match data {
-        WorkspaceType::Regular(reg_workspace_num) => {
+        WorkspaceType::Regular(_) => {
             debug!(
-                "Workspace change (Regular) to workspace {:?}",
-                reg_workspace_num
+                "WorkspaceChange event (Regular) detected, attempting to change background image"
             );
-
             let mut rng = thread_rng();
-            let chosen_background = loop {
-                match valid_image_paths.choose(&mut rng) {
-                    Some(selected_file) => {
-                        debug!("Selected (PathBuf): {:?}", selected_file);
+            let chosen_file = match valid_image_paths.choose(&mut rng) {
+                Some(selected_file) => selected_file,
+                None => {
+                    error!("Couldn't select a file from array of valid images. Exiting");
+                    panic!()
+                }
+            };
 
-                        let path = match selected_file.as_path().to_str() {
-                            Some(path) => {
-                                debug!("Converted (&str): {}", path);
-                                path
-                            }
-                            None => {
-                                warn!("Could not convert PathBuf to &str, selecting new file");
-                                continue;
-                            }
-                        };
-
-                        break path;
-                    }
-                    None => {
-                        warn!("Couldn't select a file. Attempting to select another file");
-                        continue;
-                    }
-                };
+            let path_of_chosen_file = match chosen_file.as_path().to_str() {
+                Some(path_of_image) => path_of_image,
+                None => {
+                    error!("Couldn't convert path of selected file. Exiting");
+                    panic!()
+                }
             };
 
             if let Err(_) = Command::new("swww")
-                .args(["img", chosen_background])
+                .args(["img", path_of_chosen_file])
                 .output()
             {
-                error!("Failed to issue 'swww' command. Not changing background.")
+                error!("Failed to issue 'swww' command. Not changing background")
             }
         }
         WorkspaceType::Special(_) => {
-            debug!("Workspace change event (Special) ignored");
+            debug!("WorkspaceChange event (Special) ignored");
         }
     }
 }
@@ -76,7 +66,6 @@ fn main() {
     }
     debug!("'{SWWW_BINARY}' binary found on PATH");
 
-    // Parse args and check provided dir exists
     let args = Args::parse();
     let backgrounds_dir = PathBuf::from(&args.backgrounds_path);
     if !backgrounds_dir.exists() | !backgrounds_dir.is_dir() {
@@ -87,19 +76,20 @@ fn main() {
     let valid_image_file_paths_in_provided_dir = match get_valid_image_paths_from_provided_dir(
         args.backgrounds_path,
     ) {
-        Ok(vec_image_paths) => vec_image_paths,
-        // Err(e) => panic!("Error: {e}"),
+        Ok(vec_image_paths) => {
+            debug!(
+                "Number of valid images in provided directory: {}",
+                vec_image_paths.len()
+            );
+            vec_image_paths
+        }
         Err(e) => match e {
-            utils::ParsingError::PatternError(_) => {
+            ParsingError::PatternError(_) => {
                 error!("Failed to extract Paths from provided directory. Please provide a valid directory path");
                 return;
             }
-            utils::ParsingError::NoValidFilesError(e) => {
-                error!("{e}");
-                return;
-            }
-            utils::ParsingError::NoValidImageFilesError(_) => {
-                error!("{e}");
+            ParsingError::NoValidImageFilesError => {
+                error!("Didn't find any valid image files in provided directory. Please provide a valid directory path");
                 return;
             }
         },
